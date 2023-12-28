4f8454e48a408667675d227cf426963e191194da
miladlotfi <miladlotfi66@gmail.com>
2023-12-28 16:25:01 +0330

    make card component

--- component/templates/MenuPage.js
+++ component/templates/MenuPage.js
@@ -1,12 +1,11 @@
 import styles from './MenuPage.module.css'
-
+import Card from '../modules/Card'
 function MenuPage({data}) {
-    console.log(data);
   return (
     <div className={styles.container}>
         <h2>menu</h2>
         <div className={styles.subContainer}>{
-            data.map(food=><p key={food.id}>{food.name}</p>)
+            data.map((food)=><Card key={food.id} {...food}></Card>)
             }</div>
     </div>
   )
