0e7cf94506f285f6dbd43aef8e8d5fa34a2aff0c
miladlotfi <miladlotfi66@gmail.com>
2023-12-28 18:20:30 +0330

    fetch data in dinamic page

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,23 +1,45 @@
-import React from "react";
+import { useRouter } from "next/router";
 
-function Details() {
+function Details({data}) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h2>درحال بارگذاری</h2>;
+  }
   return <div>Details</div>;
 }
 
 export default Details;
 
 export async function getStaticPaths() {
-  const res = await fetch("http://localhost/4000/data");
+  const res = await fetch("http://localhost:4000/data");
   const json = await res.json();
   const data = json.slice(0, 10);
   const paths = data.map((food) => ({
     params: {
-      id: food.id.tostring()
+      id: food.id.toString(),
     },
   }));
 
   return {
-paths,
-fallback:true
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { id },
+  } = context;
+  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+    revalidate: 10,
   };
 }
